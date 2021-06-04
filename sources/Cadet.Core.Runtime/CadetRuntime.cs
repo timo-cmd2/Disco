49c8c1b52decdec8af89dfa9c81dc74c620aa20c
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-06-04 15:57:43 +0200

    Update CadetRuntime.cs

--- sources/Cadet.Core.Runtime/CadetRuntime.cs
+++ sources/Cadet.Core.Runtime/CadetRuntime.cs
@@ -1,2 +1,14 @@
 using System;
 using System.Runtime.CompilerServices;
+
+/**
+ * <Summary>
+ *   CadetRuntime is the main entrypoint
+ *   for the Cadet programming-language
+ * </Summary>
+ */
+namespace CadetRuntime {
+  public class CadetRuntime {
+    public CadetRuntime {} 
+  }
+}
