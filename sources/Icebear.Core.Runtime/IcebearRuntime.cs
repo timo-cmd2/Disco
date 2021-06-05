4833bb5c12d8e6aa32be96bed7b0adb594e53b37
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-06-05 15:00:06 +0200

    Update and rename sources/Cadet.Core.Runtime/CadetRuntime.cs to sources/Icebear.Core.Runtime/IcebearRuntime.cs

---
+++ sources/Icebear.Core.Runtime/IcebearRuntime.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Runtime.CompilerServices;
+
+/**
+ * <Summary>
+ *   IcebearRuntime is the main entrypoint
+ *   for the Icebear programming-language
+ * </Summary>
+ */
+namespace IcebearRuntime {
+  public class IcebearRuntime {
+    public IcebearRuntime {} 
+  }
+}
